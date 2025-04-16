9cc35e64931cc66e3d048616a48c8b8a95bde794
montaser <montasergohar@gmail.com>
2025-04-17 00:31:04 +0200

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+
+  async rewrites() {
+    return [
+      {
+        source: "/en/:path*",
+        destination: "/en/:path*", // إعادة توجيه للصفحات باللغة الإنجليزية
+      },
+      {
+        source: "/ar/:path*",
+        destination: "/ar/:path*", // إعادة توجيه للصفحات باللغة العربية
+      },
+      {
+        source: "/:path*",
+        destination: "/en/:path*", // إعادة توجيه لجميع المسارات غير المعروفة إلى اللغة الافتراضية
+      },
+    ];
+  },
 };
 
 export default nextConfig;
