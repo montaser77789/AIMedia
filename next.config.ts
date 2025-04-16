7ad4413b2b0e805ccd89556029a1f8d491dde8ef
montaser <montasergohar@gmail.com>
2025-04-17 00:40:29 +0200

    Update next.config.js for i18n and rewrites

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  i18n: {
+    locales: ['en', 'ar'],
+    defaultLocale: 'en',
+  },
   async rewrites() {
     return [
       {
