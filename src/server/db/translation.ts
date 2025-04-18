e3f7ef7047116767b2680ad868c0d3805589de65
montaser <montasergohar@gmail.com>
2025-04-18 23:13:18 +0200

    Update database

--- src/server/db/translation.ts
+++ src/server/db/translation.ts
@@ -4,6 +4,7 @@ import { cache } from "../cache";
 export const getBlogs = cache(
   async (locale: "en" | "ar") => {
     const blogs = await db.blogs.findMany();
+    
 
     return blogs.map((blog) => ({
       id: blog.id,
