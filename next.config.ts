6fbab8e32eb1ee91a398a921214a10a8012d6238
montaser <montasergohar@gmail.com>
2025-04-19 00:28:08 +0200

    Add

--- next.config.ts
+++ next.config.ts
@@ -11,30 +11,6 @@ const nextConfig: NextConfig = {
   },
   basePath: "",
 
-  async rewrites() {
-    return [
-      // إعادة توجيه المسارات API (تأكد من أن API تعمل بشكل صحيح)
-      {
-        source: "/api/:path*",
-        destination: "/api/:path*", 
-      },
-      // مسارات اللغة الإنجليزية
-      {
-        source: "/en/:path*",
-        destination: "/en/:path*", 
-      },
-      // مسارات اللغة العربية
-      {
-        source: "/ar/:path*",
-        destination: "/ar/:path*", 
-      },
-      // إعادة توجيه جميع المسارات غير المعروفة إلى اللغة الافتراضية (الإنجليزية)
-      {
-        source: "/:path*",
-        destination: "/en/:path*", 
-      },
-    ];
-  },
 
 
 };
